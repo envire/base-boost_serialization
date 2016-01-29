efed1d9423f1f949539ecabc70090d56c6ed6059
Anna Born <anna.born@dfki.de>
2016-01-29 15:12:48 +0100

    add test for matrix and quaternion serialization

--- test/test_serialization.cpp
+++ test/test_serialization.cpp
@@ -1,11 +1,48 @@
 #include <boost/test/unit_test.hpp>
 
+#include <Eigen/Geometry>
+
+#include <boost/archive/polymorphic_text_iarchive.hpp>
+#include <boost/archive/polymorphic_text_oarchive.hpp>
+#include <boost/archive/polymorphic_binary_iarchive.hpp>
+#include <boost/archive/polymorphic_binary_oarchive.hpp>
+
 #include <boost_serialization/EigenTypes.hpp>
 
 using namespace boost::serialization;
 
-BOOST_AUTO_TEST_CASE(test_eigen_serialization)
+BOOST_AUTO_TEST_CASE(eigen_quaternion_serialization_test)
 {
-    Eigen::Affine3d trans = Eigen::Affine3d::Identity();
+    Eigen::Quaterniond rot = Eigen::AngleAxisd(0.45 * M_PI, Eigen::Vector3d::UnitZ()) *
+                            Eigen::AngleAxisd(0.5 * M_PI, Eigen::Vector3d::UnitY()) *
+                            Eigen::AngleAxisd(0.27 * M_PI, Eigen::Vector3d::UnitX());
 
+    std::stringstream stream;
+    boost::archive::polymorphic_binary_oarchive oa(stream);
+    oa << rot;
+
+    // deserialize from string stream
+    boost::archive::polymorphic_binary_iarchive ia(stream);
+    Eigen::Quaterniond rot_2;
+    ia >> rot_2;
+
+    BOOST_CHECK(rot.matrix() == rot_2.matrix());
+}
+
+BOOST_AUTO_TEST_CASE(eigen_matrix_serialization_test)
+{
+    Eigen::Matrix3d m = 0.2 * Eigen::Matrix3d::Identity();
+
+    std::stringstream stream;
+    boost::archive::polymorphic_binary_oarchive oa(stream);
+    oa << m;
+
+    // deserialize from string stream
+    boost::archive::polymorphic_binary_iarchive ia(stream);
+    Eigen::MatrixXd m2;
+    ia >> m2;
+
+    BOOST_CHECK(m.rows() == m2.rows());
+    BOOST_CHECK(m.cols() == m2.cols());
+    BOOST_CHECK(m == m2);
 }
