0e545068cbcfad97e6864a86f7cee0db78fd46fd
Anna Born <anna.born@dfki.de>
2016-04-26 16:09:34 +0200

    add serialization for base numeric PlaneFitting

---
+++ src/BaseNumeric.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <boost/serialization/array.hpp>
+#include <boost/serialization/nvp.hpp>
+
+#include <numeric/PlaneFitting.hpp>
+
+namespace boost { namespace serialization
+{
+
+    /** Serializes base::TransformWithCovariance */
+    template<typename _Archive,typename ScalarType>
+    inline void serialize(
+        _Archive & ar,
+        base::PlaneFitting<ScalarType>& pf,
+        const unsigned int version)
+    {
+        ar & boost::serialization::make_nvp("x", pf.x);
+        ar & boost::serialization::make_nvp("y", pf.y);
+        ar & boost::serialization::make_nvp("z", pf.z);
+        ar & boost::serialization::make_nvp("xx", pf.xx);
+        ar & boost::serialization::make_nvp("yy", pf.yy);
+        ar & boost::serialization::make_nvp("xy", pf.xy);
+        ar & boost::serialization::make_nvp("xz", pf.xz);
+        ar & boost::serialization::make_nvp("yz", pf.yz);
+        ar & boost::serialization::make_nvp("zz", pf.zz);
+        ar & boost::serialization::make_nvp("n", pf.n);
+    }
+
+}}
